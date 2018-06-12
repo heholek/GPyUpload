9813fa403bcccc17432d082b362cab15e9fd907d
Aaron Grisez <grise100@mail.chapman.edu>
2018-06-12 19:27:27 -0400

    Renamed classes for building and delivering reports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Entrypoint and primary script for the application
+Entrypoint and primary class for the application
 
 
 """
@@ -8,14 +8,14 @@ import os
 import src.UI
 import src.Authenticate as auth
 import src.ImportData as data
-import src.BuildReports as build
-import src.DeliverReports as deliver
+import src.ReportBuilder as ReportBuilder
+import src.ReportDeliverer as ReportDeliverer
 print("Imports Successful")
 
 class EvaluationsApp():
 
     def __init__(self):
-        pass
+        self.allRecords = [] #This list will contain all imported records
 
     def authenticate(self, authenticationOptions):
         """
@@ -31,7 +31,7 @@ class EvaluationsApp():
 
     def importData(self, importOptions):
         """
-        Pulls remote data for reporting
+        Pulls remote data for reporting, stores it internally as a list of JuryRecord objects
         """
         pass
 
@@ -40,6 +40,8 @@ class EvaluationsApp():
         Assembles reports and renders pdfs
 
         After this function, user can explore the temp directory to find all reports currently staged for delivery
+
+        Iterate through self.allRecords and call BuildReport
         """
         pass
 
---
+++ src/ReportBuilder.py
@@ -0,0 +1,19 @@
+
+class ReportBuilder():
+    """
+    Creates a pdf when .buildReport() is called and passed a record object
+
+    On building and initializing the ReportBuilder, certain defaults are set
+    """
+
+    def __init__(self):
+        """
+        Set Options
+        """
+
+    def buildReport(self, report):
+        """
+        Receives a report object and parses into the appropriate template
+        """
+
+print("Successfully loaded ReportBuilder")
